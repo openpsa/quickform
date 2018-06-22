95c9b178bb7ebba4689cf82dabf28f242d44318c
MaybeFactor <MaybeFactor@github.com>
2018-06-22 14:15:25 +0930

    Make "compare" validation rule treat all values as strings

--- lib/HTML/QuickForm/Rule/Compare.php
+++ lib/HTML/QuickForm/Rule/Compare.php
@@ -59,9 +59,9 @@ class HTML_QuickForm_Rule_Compare extends HTML_QuickForm_Rule
     {
         switch ($this->_findOperator($operator)) {
             case '===':
-                return floatval($values[0]) === floatval($values[1]);
+            	return strval($values[0]) === strval($values[1]);
             case '!==':
-                return floatval($values[0]) !== floatval($values[1]);
+            	return strval($values[0]) !== strval($values[1]);
             case '>':
                 return strval($values[0]) > strval($values[1]);
             case '>=':
